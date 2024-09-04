50737f372323f8fdec1324ef3904ae25ab3e892b
akshatmaheshwari <a.maheshwari@thesynapses.com>
2024-09-04 19:11:29 +0530

    added formatting

--- client/src/hooks/useListenMessages.js
+++ client/src/hooks/useListenMessages.js
@@ -6,18 +6,18 @@ import useConversation from "../zustand/useConversations.js";
 import notificationSound from "../assets/Sound/sound.mp3";
 
 const useListenMessages = () => {
-	const { socket } = useSocketContext();
-	const { messages, setMessages } = useConversation();
+  const { socket } = useSocketContext();
+  const { messages, setMessages } = useConversation();
 
-	useEffect(() => {
-		socket?.on("newMessage", (newMessage) => {
-			newMessage.shouldShake = true;
-			const sound = new Audio(notificationSound);
-			sound.play();
-			setMessages([...messages, newMessage]);
-		});
+  useEffect(() => {
+    socket?.on("newMessage", (newMessage) => {
+      newMessage.shouldShake = true;
+      const sound = new Audio(notificationSound);
+      sound.play();
+      setMessages([...messages, newMessage]);
+    });
 
-		return () => socket?.off("newMessage");
-	}, [socket, setMessages, messages]);
+    return () => socket?.off("newMessage");
+  }, [socket, setMessages, messages]);
 };
 export default useListenMessages;
